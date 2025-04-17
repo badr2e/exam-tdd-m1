63a358b0c247a3cacfe5b21744e35e4b48ef8875
badr2e <badreprocontact@gmail.com>
2025-04-17 09:19:49 +0200

    Initialize Spring Boot project with Maven wrapper and basic structure

---
+++ src/main/java/com/examtdd/examtdd/ExamtddApplication.java
@@ -0,0 +1,13 @@
+package com.examtdd.examtdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamtddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamtddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examtdd/examtdd/ExamtddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examtdd.examtdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamtddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
